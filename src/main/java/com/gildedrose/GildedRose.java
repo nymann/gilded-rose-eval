0b14e790f0e728172a555d4487621982ea62a98d
Hivemind Drone <hivemind-drone@noreply.github.com>
2026-04-08 20:04:27 +0000

    refactor: extract updateQualityAfterSellBy method to isolate post-sell-by logic

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -48,20 +48,24 @@ class GildedRose {
         }
 
         if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
+            updateQualityAfterSellBy(item);
+        }
+    }
+
+    private void updateQualityAfterSellBy(Item item) {
+        if (!item.name.equals("Aged Brie")) {
+            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
                     }
-                } else {
-                    item.quality = item.quality - item.quality;
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+                item.quality = item.quality - item.quality;
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
             }
         }
     }
