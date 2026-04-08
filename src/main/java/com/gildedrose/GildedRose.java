fa732d7b459a81c311fce1a51c94c1b0c8b5ab91
Hivemind Drone <hivemind-drone@noreply.github.com>
2026-04-08 16:27:21 +0000

    feat: conjured item quality decreases by 4 after sell-by date and never below 0

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -7,15 +7,19 @@ class GildedRose {
         this.items = items;
     }
 
+    private void decrementQuality(Item item) {
+        item.quality = item.quality - 1;
+        if (item.name.equals("Conjured Mana Cake") && item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+    }
+
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                        if (items[i].name.equals("Conjured Mana Cake") && items[i].quality > 0) {
-                            items[i].quality = items[i].quality - 1;
-                        }
+                        decrementQuality(items[i]);
                     }
                 }
             } else {
@@ -47,7 +51,7 @@ class GildedRose {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+                                decrementQuality(items[i]);
                             }
                         }
                     } else {
