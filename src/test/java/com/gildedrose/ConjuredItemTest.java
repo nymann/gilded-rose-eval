e683e2914e77dc53f275946d6a7cc8f33a0cfdf9
Hivemind Drone <hivemind-drone@noreply.github.com>
2026-04-08 16:28:16 +0000

    feat: conjured item quality never goes below 0 after sell-by date

--- src/test/java/com/gildedrose/ConjuredItemTest.java
+++ src/test/java/com/gildedrose/ConjuredItemTest.java
@@ -37,6 +37,16 @@ public class ConjuredItemTest {
     }
 
     @Test
+    void conjuredItemQualityNeverGoesBelowZeroAfterSellByDate() {
+        Item item = new Item("Conjured Mana Cake", 0, 3);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(0, item.quality);
+    }
+
+    @Test
     void conjuredItemQualityDecreasesByFourAfterSellByDate() {
         Item item = new Item("Conjured Mana Cake", 0, 10);
         GildedRose gildedRose = new GildedRose(new Item[]{item});
