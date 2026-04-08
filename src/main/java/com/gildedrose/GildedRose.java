107222a45517ee6ee47a3ca0af2580193407ce4c
Hivemind Drone <hivemind-drone@noreply.github.com>
2026-04-08 19:00:13 +0000

    refactor: replace quality self-subtraction with explicit assignment to zero

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -43,7 +43,7 @@ class GildedRose {
                             }
                         }
                     } else {
-                        item.quality = item.quality - item.quality;
+                        item.quality = 0;
                     }
                 } else {
                     if (item.quality < 50) {
