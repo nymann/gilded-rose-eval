f5cc2253363ecd318abec35829d545edf8bea0b8
Hivemind Drone <hivemind-drone@noreply.github.com>
2026-04-08 19:18:19 +0000

    refactor: extract increaseQuality, isConjured helpers and use ++/-- operators

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -22,7 +22,7 @@ class GildedRose {
         updateQualityBeforeSellByDate(item);
 
         if (!isSulfuras(item)) {
-            item.sellIn = item.sellIn - 1;
+            item.sellIn--;
         }
 
         if (item.sellIn < 0) {
@@ -32,22 +32,14 @@ class GildedRose {
 
     private void updateQualityBeforeSellByDate(Item item) {
         if (item.name.equals(AGED_BRIE)) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-            }
+            increaseQuality(item);
         } else if (item.name.equals(BACKSTAGE_PASSES)) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-                if (item.sellIn < 11) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-                if (item.sellIn < 6) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
+            increaseQuality(item);
+            if (item.sellIn < 11) {
+                increaseQuality(item);
+            }
+            if (item.sellIn < 6) {
+                increaseQuality(item);
             }
         } else if (!isSulfuras(item)) {
             decreaseQuality(item);
@@ -56,9 +48,7 @@ class GildedRose {
 
     private void updateQualityAfterSellByDate(Item item) {
         if (item.name.equals(AGED_BRIE)) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-            }
+            increaseQuality(item);
         } else if (item.name.equals(BACKSTAGE_PASSES)) {
             item.quality = 0;
         } else if (!isSulfuras(item)) {
@@ -66,15 +56,25 @@ class GildedRose {
         }
     }
 
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+    }
+
     private boolean isSulfuras(Item item) {
         return item.name.equals(SULFURAS);
     }
 
+    private boolean isConjured(Item item) {
+        return item.name.equals(CONJURED);
+    }
+
     private void decreaseQuality(Item item) {
         if (item.quality > 0) {
-            item.quality = item.quality - 1;
-            if (item.name.equals(CONJURED) && item.quality > 0) {
-                item.quality = item.quality - 1;
+            item.quality--;
+            if (isConjured(item) && item.quality > 0) {
+                item.quality--;
             }
         }
     }
