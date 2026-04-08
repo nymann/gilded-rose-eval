bba3e4f9e7a721797f8a98440d8d23dd2120878f
Hivemind Drone <hivemind-drone@noreply.github.com>
2026-04-08 18:54:27 +0000

    feat: support Conjured item quality decreasing by 2 per day before sell-by date

---
+++ src/test/java/com/gildedrose/ConjuredItemTest.java
@@ -0,0 +1,17 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConjuredItemTest {
+    @Test
+    void givenConjuredItemBeforeSellByDateQualityDecreasesByTwoPerDay() {
+        Item item = new Item("Conjured Mana Cake", 5, 10);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(8, item.quality);
+    }
+}
