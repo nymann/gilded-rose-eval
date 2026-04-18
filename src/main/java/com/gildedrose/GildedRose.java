fd499878e142003f2a02bba35f48794f7c17b419
Kristian Jakobsen <kristian@nymann.dev>
2026-04-18 12:13:11 +0200

    feat: oracle stone advances day counter and preserves quality each update

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,6 +9,11 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+            if (items[i].name.equals("Oracle Stone")) {
+                items[i].sellIn = items[i].sellIn + 1;
+                continue;
+            }
+
             if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
