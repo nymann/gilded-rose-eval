208cb632c27f30359c6a5804ffa7d65e62f0cf9d
Kristian Jakobsen <kristian@nymann.dev>
2026-04-18 13:26:32 +0200

    feat: add Oracle Stone item that increments day counter without changing quality

---
+++ src/test/java/com/gildedrose/OracleStoneTest.java
@@ -0,0 +1,19 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OracleStoneTest {
+
+    @Test
+    void unsealedOracleStoneOnOrdinaryDayAdvancesOnlyTheDayCounter() {
+        Item item = new Item("Oracle Stone", 1, 20);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(2, item.sellIn);
+        assertEquals(20, item.quality);
+    }
+}
