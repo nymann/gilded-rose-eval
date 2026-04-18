96d47a5744bca994788d59ca8e2ca5d8d444370a
Kristian Jakobsen <kristian@nymann.dev>
2026-04-18 12:44:33 +0200

    feat: favorable oracle blessing on every seventh day adds ten quality

--- src/main/java/com/gildedrose/OracleStone.java
+++ src/main/java/com/gildedrose/OracleStone.java
@@ -1,12 +1,20 @@
 package com.gildedrose;
 
+import java.util.function.DoubleSupplier;
+
 public class OracleStone extends Item {
     public int day;
     public boolean sealed;
+    public final DoubleSupplier roller;
 
     public OracleStone(int day, int quality, boolean sealed) {
+        this(day, quality, sealed, Math::random);
+    }
+
+    public OracleStone(int day, int quality, boolean sealed, DoubleSupplier roller) {
         super("Oracle Stone", day, quality);
         this.day = day;
         this.sealed = sealed;
+        this.roller = roller;
     }
 }
