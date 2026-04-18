2de02daabb9a7b481ff0fa87d97b0bfa20cebc57
Kristian Jakobsen <kristian@nymann.dev>
2026-04-18 11:56:51 +0200

    refactor: replace conditional dispatch with Strategy pattern in updateQuality

---
+++ src/main/java/com/gildedrose/AgedBrieUpdater.java
@@ -0,0 +1,14 @@
+package com.gildedrose;
+
+class AgedBrieUpdater implements ItemUpdater {
+    @Override
+    public void update(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+        item.sellIn--;
+        if (item.sellIn < 0 && item.quality < 50) {
+            item.quality++;
+        }
+    }
+}
---
+++ src/main/java/com/gildedrose/BackstagePassUpdater.java
@@ -0,0 +1,20 @@
+package com.gildedrose;
+
+class BackstagePassUpdater implements ItemUpdater {
+    @Override
+    public void update(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+            if (item.sellIn < 11 && item.quality < 50) {
+                item.quality++;
+            }
+            if (item.sellIn < 6 && item.quality < 50) {
+                item.quality++;
+            }
+        }
+        item.sellIn--;
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+    }
+}
