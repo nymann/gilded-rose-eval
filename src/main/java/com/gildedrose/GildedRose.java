c461aca5ddeb43d6871ae49102103d969b4d3c1a
Kristian Jakobsen <kristian@nymann.dev>
2026-04-18 12:54:09 +0200

    feat: Oracle Stone advances day counter and preserves quality

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,7 +11,7 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros") && !items[i].name.equals("Oracle Stone")) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
@@ -35,7 +35,9 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (items[i].name.equals("Oracle Stone")) {
+                items[i].sellIn = items[i].sellIn + 1;
+            } else if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
---
+++ src/test/java/com/gildedrose/OracleStoneTest.java
@@ -0,0 +1,20 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class OracleStoneTest {
+
+    @Test
+    void givenUnsealedOracleStoneOnOrdinaryDayWhenDayPassesThenOnlyDayCounterAdvances() {
+        Item item = new Item("Oracle Stone", 1, 20);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(2, item.sellIn);
+        assertEquals(20, item.quality);
+    }
+}
