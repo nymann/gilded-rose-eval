7df57788895f8cf4414829d67c0f47568d01b51c
Kristian Jakobsen <kristian@nymann.dev>
2026-04-18 13:58:12 +0200

    feat: add OracleStone with day counter advancing on update

---
+++ src/main/java/com/gildedrose/OracleStone.java
@@ -0,0 +1,17 @@
+package com.gildedrose;
+
+public class OracleStone {
+    public int day;
+    public int quality;
+    public boolean sealed;
+
+    public OracleStone(int day, int quality, boolean sealed) {
+        this.day = day;
+        this.quality = quality;
+        this.sealed = sealed;
+    }
+
+    public void update() {
+        day++;
+    }
+}
